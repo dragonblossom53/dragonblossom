abc4dbec5d47139e037a563f60943340593f0ba8
dragonblossom53 <dragonblossom53@gmail.com>
2024-12-20 16:16:21 -0700

    Update walker-animation.js

--- walker-animation.js
+++ walker-animation.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('#canvas2');
+const canvas = document.getElementById('myCanvas2');
 const ctx = canvas.getContext('2d');
 const width = canvas.width = 320;
 const height = canvas.height = 480;
