6fd831d02aa7d6f1ef6da47bb5bb2ecddb7ba656
dragonblossom53 <dragonblossom53@gmail.com>
2024-12-20 14:13:14 -0700

    Update walkers.js
    remove sprite to put in its own folder

--- walkers.js
+++ walkers.js
@@ -33,40 +33,6 @@
         let blockOffsetLeft = 100;
         let blocks = []
         let blocks2 = []
-
-        //trying to figure out sprite animation
-        const spriteSheet = new Image();
-        spriteSheet.src = "spritesheet-walker.png";
-
-        function drawFrame(x, y, width, height) {
-        ctx.clearRect(0,0, canvas.width, canvas.height);
-        ctx.drawImage(spriteSheet, x, y, width, height);
-        }
-
-        /*spriteSheet.onLoad = function() {
-                init();
-        }
-
-        const frameWidth = 35;
-        const frameHeight = 75;
-        const frameCount = 10;
-        let currentFrame = 0;
-
-        function drawFrame(frameX, frameY, canvasX, canvasY) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(spriteSheet, frameX * frameWidth, frameY * frameHeight, frameWidth, frameHeight, canvasX, canvasY, frameWidth, frameHeight);
-        }
-
-        function animateFrames() {
-        drawFrame(currentFrame, 0, 0, 0);
-        currentFrame = (currentFrame + 1) % frameCount;
-        requestAnimationFrame(animateFrames);
-        }
-
-        function init() {
-        animateFrames();
-        }*/
-        //end of sprite animation
                 
         class Vector {
 
