5952d099765223da2d20b2e7f5b6d3ba6f88bed8
dragonblossom53 <dragonblossom53@gmail.com>
2024-12-20 13:59:32 -0700

    Update walkers.js

--- walkers.js
+++ walkers.js
@@ -34,9 +34,35 @@
         let blocks = []
         let blocks2 = []
 
+        //trying to figure out sprite animation
         const spriteSheet = new Image();
-        spriteSheet.src = "images"
+        spriteSheet.src = "spritesheet-walker.png";
 
+        spriteSheet.onLoad = function() {
+                init();
+        }
+
+        const frameWidth = 35;
+        const frameHeight = 75;
+        const frameCount = 10;
+        let currentFrame = 0;
+
+        function drawFrame(frameX, frameY, canvasX, canvasY) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(spriteSheet, frameX * frameWidth, frameY * frameHeight, frameWidth, frameHeight, canvasX, canvasY, frameWidth, frameHeight);
+        }
+
+        function animateFrames() {
+        drawFrame(currentFrame, 0, 0, 0);
+        currentFrame = (currentFrame + 1) % frameCount;
+        requestAnimationFrame(animateFrames);
+        }
+
+        function init() {
+        animateFrames();
+        }
+        //end of sprite animation
+                
         class Vector {
 
             constructor(x, y) {
